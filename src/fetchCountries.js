ce0ff0056685db8e2546b63556fd3960c4829823
Tetiana1192 <tanya_strigun@ukr.net>
2022-05-26 02:53:59 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,5 @@
 export default function fetchCountries(name) {
-  const BASE_URL = 'https://restcountries.com/v2.1/';
+  const BASE_URL = 'https://restcountries.com/v3.1/';
 
   return fetch(`${BASE_URL}name/${name}?fields=name,capital,population,flags,languages`).then(
     response => {
